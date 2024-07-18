78ae5b2040697375015b4671d9bf83d900b0b9b3
Prathamesh72003 <pthakare72003@gmail.com>
2024-07-19 03:41:34 +0530

    patient home and carousel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,13 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import PatientHome from './Components/PatientHome'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import CareTakersInfo from './Components/CareTakersInfo'
-import Register from './Components/Register'
-import MapComponent from './Components/MapComponent'
-import Calendar from './Components/Calender'
-import Corousel from './Components/Corousel'
-
-const App = () => {
-    return(
-        <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' exact element={<CareTakersInfo/>}></Route>
-                    <Route path='/register' exact element={<Register/>}></Route>
-                    <Route path='/register' exact element={<Register/>}></Route>
-                    <Route path='/map' exact element={<MapComponent/>}></Route>
-                    <Route path='/calender' exact element={<Calendar/>}></Route>
-                    <Route path='/corousel' exact element={<Corousel/>}></Route>
-                </Routes>
-            </BrowserRouter>
-        </div>
-    )
+function App() {
+  return (
+    <>
+    <PatientHome />
+    </>
+  )
 }
 
 export default App
-
---
+++ frontend/src/Components/PatientHome.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import CarouselComponent from './CarouselComponent';
+import './PatientHome.css';
+
+function PatientHome() {
+  const btnArray = [
+    { name: "Contact", routeAddr: "route1" },
+    { name: "Quiz", routeAddr: "route1" },
+    { name: "Meditation", routeAddr: "route1" },
+    { name: "Solve My Confusion", routeAddr: "route1" }
+  ];
+
+  return (
+    <div className='container'>
+      <div className='upperContainer'>
+        <CarouselComponent />
+      </div>
+      <div className='lowerContainer'>
+        <div className='gridButtons'>
+          {btnArray.map((item, index) => {
+            return (
+              <button type='button' key={index}>{item.name}</button>
+            );
+          })}
+        </div>
+        <button type="button">SOS</button>
+      </div>
+    </div>
+  );
+}
+
+export default PatientHome;
