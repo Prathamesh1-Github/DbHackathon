085b1e2bd1681f04a7e551b39b93217f9e2e0fa2
PrathameshDhorage <prathameshdhorage@gmail.com>
2024-07-19 06:19:36 +0530

    add routing done and pages required for it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,18 @@ import Register from './Components/Register'
 import MapComponent from './Components/MapComponent'
 import Calendar from './Components/Calender'
 import Corousel from './Components/Corousel'
+import Start from './Components/Start'
+import CareTakerHome from './Components/CareTakerHome'
+
 
 const App = () => {
     return(
         <div>
             <BrowserRouter>
                 <Routes>
-                    <Route path='/' exact element={<CareTakersInfo/>}></Route>
-                    <Route path='/register' exact element={<Register/>}></Route>
+                    <Route path='/' exact element={<Start/>}></Route>
+                    <Route path='/caretaker' exact element={<CareTakerHome/>}></Route>
+                    <Route path='/caretaker-register' exact element={<CareTakersInfo/>}></Route>
                     <Route path='/register' exact element={<Register/>}></Route>
                     <Route path='/map' exact element={<MapComponent/>}></Route>
                     <Route path='/calender' exact element={<Calendar/>}></Route>
